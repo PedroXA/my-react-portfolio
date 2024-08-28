2869ff1dfca4a436942086e4164f6d767948281d
PedroXA <pedroxad@gmail.com>
2024-08-28 14:30:36 -0300

    add: attached hero section to mainPage jsx file

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar/navbar.jsx'
 import Header from '../components/Header/header.jsx'
+import Hero from '../components/Hero/hero.jsx'
 
 const mainPage = () => {
     return (
       <div>
         <Navbar/>
         <Header/>
+        <Hero/>
       </div>
     );
   };
