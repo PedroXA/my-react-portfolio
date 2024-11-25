e726a95fa674ccb271c24c83e7e193f886d66f9d
PedroXA <pedroxad@gmail.com>
2024-11-25 14:04:31 -0300

    add: eslint adition

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parser: '@typescript-eslint/parser', // Define o parser para TypeScript
+  parserOptions: {
+    ecmaVersion: 2020, // Define a versão do ECMAScript
+    sourceType: 'module', // Define o uso de módulos ECMAScript
   },
-}
+  extends: [
+    'eslint:recommended', // Regras recomendadas pelo ESLint
+    'plugin:@typescript-eslint/recommended', // Regras recomendadas para TypeScript
+    'plugin:prettier/recommended', // Integra com Prettier
+  ],
+  plugins: [
+    '@typescript-eslint', // Usado para linting do TypeScript
+    'prettier', // Integra Prettier com ESLint
+  ],
+  rules: {
+    // Regras personalizadas
+    'no-console': 'warn', // Aviso ao invés de erro para console.log
+    'no-debugger': 'warn', // Aviso ao invés de erro para debugger
+    '@typescript-eslint/no-unused-vars': ['warn'], // Aviso para variáveis não usadas
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5' }], // Integra com Prettier
+    'eqeqeq': ['warn', 'always'], // Força o uso de `===` ao invés de `==`
+    'semi': ['error', 'always'], // Exige ponto e vírgula
+    'quotes': ['error', 'single'], // Exige o uso de aspas simples
+    'indent': ['error', 2], // Força indentação de 2 espaços
+  },
+};
